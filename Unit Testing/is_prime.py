c9efcb9f76f84b009808e1647c305cfeeb9aed70
0merjavaid <14besemjavaid@seecs.edu.pk>
2017-09-19 12:32:36 +0500

    python unit testing demonstration

--- Unit Testing/is_prime.py
+++ Unit Testing/is_prime.py
@@ -10,8 +10,8 @@ def is_prime_correct(number):
     """Return True if *number* is prime."""
     if(number<=1):
 	return False
-    for element in range(2,number):
-        if number % element == 0: # divide by zero error
+    for element in range(2,number):  #Error corrected by changing range from 0-n to 2-n
+        if number % element == 0: 
             return False
 
     return True
