fd7dc8a49cf51b7f26d8face8f2fefb5628d7640
0merjavaid <14besemjavaid@seecs.edu.pk>
2017-09-20 22:04:04 +0500

    Profiling Python

---
+++ Unit Testing/prime.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+    """Return True if *number* is prime."""
+    for element in range(number):
+        if number % element == 0: # divide by zero error
+            return False
+
+    return True
+
+def is_prime_correct(number):
+    """Return True if *number* is prime."""
+    if(number<=1):
+	return False
+    for element in range(2,number):  #Error corrected by changing range from 0-n to 2-n
+        if number % element == 0: 
+            return False
+
+    return True
---
+++ profiling/cpu_profile.py
@@ -0,0 +1,45 @@
+import numpy , cv2 
+import yappi
+  
+def invert_numpy(image): 
+	img=image.copy()
+	img=image[:,:]
+
+	 
+	return img
+
+def naive(image):
+	img=image.copy()
+	rows,cols=image.shape
+	
+	for i in range(rows):
+		for j in range(cols):
+			img[i,j]=255-image[i,j]
+	return img
+			
+def display(orig,invert):
+    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image',400,400)
+    cv2.namedWindow('image0',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image0',400,400)
+    cv2.imshow('image0',orig)
+    cv2.waitKey(0)
+    cv2.imshow('image',invert)
+    cv2.waitKey(0)
+	
+def main():
+    image=cv2.imread('image.jpg',0) 
+    invert_numpy(image)
+    invert=naive(image)
+    display(image,invert)
+    
+yappi.start()
+
+main()
+
+yappi.get_func_stats().print_all()
+
+yappi.get_thread_stats().print_all()
+print yappi.get_mem_usage()
+ 
+
